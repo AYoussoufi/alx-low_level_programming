d391d82c5a7b7bf5bc9d2cf545e69555ad5f7690
Akram Youssoufi <akram.lyoussoufi@gmail.com>
2023-06-15 14:32:39 -0700

    update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - The main method of the program
+ *
+ * This function print a message with printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+printf("with proper grammer, but the outcome is a piece of art,");
+return 0;
+}
