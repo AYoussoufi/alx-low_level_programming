447028bf3be7a803ce9846e1f1a9332adfa2605f
Akram Youssoufi <akram.lyoussoufi@gmail.com>
2023-06-15 14:27:52 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point of the program
  *
- * This function prints the message "Programming is like building a multilingual puzzle"
+ * This function prints a string
  * using the puts function and returns 0 to indicate successful execution.
  *
  * Return: Always 0 (Success)
