85bd5963f0eb1166d0d9adf5ff37ed466df61f98
Akram Youssoufi <akram.lyoussoufi@gmail.com>
2023-06-15 14:29:25 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * ain - Entry point of the program
+ * main - Entry point of the program
  *
  * This function prints a string
  * using the puts function and returns 0 to indicate successful execution.
