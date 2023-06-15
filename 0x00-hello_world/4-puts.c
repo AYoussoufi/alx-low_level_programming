e55d3ec30dea471b80cf998a05d3c5184c462ef5
Akram Youssoufi <akram.lyoussoufi@gmail.com>
2023-06-15 14:17:21 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
-int main() 
+//THIS IS THE MAIN METHOD AND PRINT HELLO WORLD
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
