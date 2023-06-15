0375a177a6eecad0c06b48c7ceab25cb4177d597
Akram Youssoufi <akram.lyoussoufi@gmail.com>
2023-06-15 14:21:54 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
-/*THIS IS THE MAIN METHOD AND PRINT HELLO WORLD*/
+/**
+ *This is the main function of the program
+ *This function print a string to the terminal
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
