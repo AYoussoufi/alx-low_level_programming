149705ac08953a4da5f9d1c04c083da2fb968905
Akram Youssoufi <akram.lyoussoufi@gmail.com>
2023-06-16 17:33:04 -0700

    update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,9 +9,15 @@
  */
 int main(void)
 {
-for (int x = '0';x != '8';x++){
-for (int y = x + 1;y != '9';y++){
-for (int z = y + 1;z != ':';z++){
+int x;
+for (x = '0'; x != '8'; x++)
+{
+int y;
+for (y = x + 1; y != '9'; y++)
+{
+int z;
+for (z = y + 1 ;z != ':'; z++)
+{
 putchar(x);
 putchar(y);
 putchar(z);
